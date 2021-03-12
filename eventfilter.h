f1bd4265ba2bb3c88d2f348e2c37e2d06986b146
David Singleton <davidsingleton@gmail.com>
2021-03-11 23:02:28 -0800

    hup

--- eventfilter.h
+++ eventfilter.h
@@ -11,15 +11,11 @@ class EventFilter : public QObject
     Q_OBJECT
 public:
     QQuickItem* root;
-    explicit EventFilter(QObject* parent = nullptr);
+    explicit EventFilter(QObject* parent = nullptr) : QObject(parent), root(nullptr) {}
 signals:
     void suspend();
 protected:
   bool eventFilter(QObject* obj, QEvent* ev);
 };
-
-EventFilter::EventFilter(QObject *parent) : QObject(parent), root(nullptr) {
-
-}
 
 #endif // EVENTFILTER_H
